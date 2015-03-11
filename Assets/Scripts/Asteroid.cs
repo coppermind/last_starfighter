2038dda63696a236b4ca80593c775ee7886d6f19
Paul Corpuz <paul.corpuz@gmail.com>
2015-03-11 23:33:03 +0800

    Added Asteroids

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+	[SerializeField]
+	private Sprite[] spriteArray;
+	
+	[SerializeField]
+	private float minGravity;
+	
+	[SerializeField]
+	private float maxGravity;
+	
+	[SerializeField]
+	private float damagePoints = 30f;
+
+	private SpriteRenderer spriteRenderer;
+	private Rigidbody2D rigidBody;
+	
+	private float screenBottomEdge;
+
+	void Start() {
+		spriteRenderer    = GetComponent<SpriteRenderer>();
+		rigidBody         = GetComponent<Rigidbody2D>();
+		
+		Camera camera     = Camera.main;
+		float distance    = transform.position.z - camera.transform.position.z;
+		screenBottomEdge  = camera.ViewportToWorldPoint(new Vector3(0, 0, distance)).y;
+		
+		SetRandomSprite();
+		SetRandomGravity();
+	}
+	
+	void Update() {
+		if (transform.position.y <= screenBottomEdge) {
+			Destroy(gameObject);
+		}
+	}
+	
+	void SetRandomSprite() {
+		int randomIndex = Random.Range(0, (spriteArray.Length - 1));
+		spriteRenderer.sprite = spriteArray[randomIndex];
+	}
+	
+	void SetRandomGravity() {
+		float gravityScale = Random.Range(minGravity, maxGravity);
+		rigidBody.gravityScale = gravityScale;
+	}
+	
+	public float DamagePoints {
+		get { return damagePoints; }
+		set { damagePoints = value; }
+	}
+}
---
+++ Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidController : MonoBehaviour {
+
+	[SerializeField]
+	GameObject asteroidPrefab;
+
+	[SerializeField]
+	private float spawnRate;
+	
+	[SerializeField]
+	private float spawnY;
+	
+	[SerializeField]
+	private float minX;
+	
+	[SerializeField]
+	private float maxX;
+	
+	void Update () {
+		float probability = spawnRate * Time.deltaTime;
+		if (Random.value < probability) {
+			Debug.Log("Spawning asteroid...");
+			Vector3 position = GetRandomPosition();
+			Instantiate(asteroidPrefab, position, Quaternion.identity);
+		}
+	}
+	
+	Vector3 GetRandomPosition() {
+		float x = Random.Range(minX, maxX);
+		return new Vector3(x, spawnY, 0f);
+	}
+}
