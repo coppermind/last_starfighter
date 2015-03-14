37e942f6f1cc9c68320b2896b732fde1b26b1cbc
Paul Corpuz <paul.corpuz@gmail.com>
2015-03-14 22:26:12 +0800

    Added text controller for in-level story text.

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	[SerializeField]
+	private string[] text;
+
+	[SerializeField]
+	private Text displayText;
+
+	private int currentTextIndex;
+
+	private GameManager gameManager;
+
+	void Start () {
+		gameManager = FindObjectOfType<GameManager>();
+		gameManager.PauseGame();
+		
+		if (0 < text.Length) {
+			currentTextIndex = 0;
+			displayText.text = text[0];
+		} else {
+			CloseTextPanel();
+		}
+	}
+	
+	public void Continue() {
+		if (currentTextIndex >= text.Length-1) {
+			CloseTextPanel();
+		}
+	}
+	
+	public void CloseTextPanel() {
+		gameObject.SetActive(false);
+		gameManager.UnpauseGame();
+	}
+}
